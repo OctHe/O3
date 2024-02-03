47542fdcca842a5489b8ed84cb19e71bd69db966
OctHe <hsy1995313@gmail.com>
2024-02-03 20:15:26 +0800

    Fix the amplitude error of channel model

--- ChannelModel.m
+++ ChannelModel.m
@@ -3,7 +3,7 @@
 % Rician Channel Model
 % Reference: IEEE 802.11n Indoor MIMO WLAN Channel Models
 %
-% Copyright (C) 2024  Shiyue He (hsy1995313@gmail.com)
+% Copyright (C) 2024 OctHe
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 classdef ChannelModel
@@ -73,7 +73,7 @@ classdef ChannelModel
                     H.nlos_shadow = 4;
                 case 'C'
                     H.rms_delay = 30;
-                    H.cluster = struct("delay", {0:10:90,
+                    H.cluster = struct("delay", {0:10:90, ...
                                         [60, 70, 80, 90, 110, 140, 170, 200]},
                                         "power", {[0, -2.1, -4.3, -6.5, -8.6, -10.8, -13.0, -15.2, -17.3, -19.5], ...
                                                 [-5, -7.2, -9.3, -11.5, -13.7, -15.8, -18, -20.2]},
@@ -125,7 +125,7 @@ classdef ChannelModel
                                         [600, 730], ...
                                         [880, 1050]},
                                         "power", {[-3.3, -3.6, -3.9, -4.2, -4.6, -5.3, -6.2, -7.1, -8.2, -9.5, -11.0, -12.1, -14.3, -16.7, -19.9], ...
-                                                [-1.8, -2.8, -3.5, -4.4, -5.3, -7.4, -7.0, -10.3, -10.4, -13.8, 15.7, -19.9], ...
+                                                [-1.8, -2.8, -3.5, -4.4, -5.3, -7.4, -7.0, -10.3, -10.4, -13.8, -15.7, -19.9], ...
                                                 [-5.7, -6.7, -10.4, -9.6, -14.1, -12.7, -18.5], ...
                                                 [-8.8, -13.3, -18.7], ...
                                                 [-12.9, -14.2], ...
@@ -143,8 +143,7 @@ classdef ChannelModel
 
             H.los_shadow = 3;
 
-            % L = H.pathloss(model, d)
-            L = 0;
+            PL = H.pathloss(model, d);
 
             Ncluster = length(H.cluster);
             Ttap = H.cluster(1).delay(1): H.Ts: (H.cluster(Ncluster).delay(end) + H.Ts);
@@ -159,17 +158,20 @@ classdef ChannelModel
                 phase_AoD = 2j * pi * fc * (Ltx_cm / 100) * cos(2 * pi * AoD / 360) / c * (0: Ntx-1).';
                 phase_AoA = 2j * pi * fc * (Lrx_cm / 100) * cos(2 * pi * AoA / 360) / c * (0: Nrx-1);
 
-                H.Doppler(icluster) = rand_Doppler(v, fc);
+                if v > 0
+                    H.Doppler(icluster) = rand_Doppler(v, fc);
+                else
+                    H.Doppler(icluster) = 0;
+                end
 
                 for itap = 1: Ntap
-                    if H.Ts * itap < clstr.delay(1)
-                        H.taps(:, :, icluster, itap) = zeros(Nrx, Ntx);
+                    if H.Ts * (itap -1) < clstr.delay(1)
                         continue;
                     endif
 
                     tap_amp = 10^(interp1(clstr.delay, clstr.power, Ttap(itap), 'extrap')/20);
 
-                    H.taps(:, :, icluster, itap) = 10^(-L / 20) * tap_amp * (...
+                    H.taps(:, :, icluster, itap) = 10^(-PL / 20) * tap_amp * (...
                                 sqrt(H.K / (H.K + 1)) * exp(2j * pi * rand(Nrx, Ntx)) .* (exp(phase_AoD) * exp(phase_AoA)) + ...
                                 sqrt(1 / (H.K + 1)) * (1 / sqrt(2) * (randn(Nrx, Ntx) + 1j * randn(Nrx, Ntx))) ...
                             );
@@ -178,19 +180,25 @@ classdef ChannelModel
             endfor
         endfunction
 
-        function L = pathloss(H, model, d)
+        % Existing model does no include path loss
+        function PL = pathloss(H, model, d)
 
-            if d <= 2
-                error("The pathloss model may be inaccurate for d <= 2 case.");
+            % The path loss when Tx-Rx distance is set as 70 dB.
+            % This is an experienced value and may be change in different environments.
+            d0 = 5;
+            PL0 = 70;
+
+            if d <= d0
+                error("The pathloss model may be inaccurate for d <= 5 case.");
             endif
 
             % The shadow is limited by 14 dB
             if d > H.breakpoint
-                L = 20 * log10(H.breakpoint) + ...
+                PL = PL0 + 20 * log10(H.breakpoint) + ...
                     35 * log10(d / H.breakpoint) + ...
                     max(H.nlos_shadow * randn(), 14);
             else
-                L = 20 * log10(d) + max(H.los_shadow * randn(), 14);
+                PL = PL0 = 20 * log10(d) + max(H.los_shadow * randn(), 14);
             endif
 
         endfunction
--- test_channel_model.m
+++ test_channel_model.m
@@ -2,13 +2,13 @@
 %
 % Plot channel model
 %
-% Copyright (C) 2024  Shiyue He (hsy1995313@gmail.com)
+% Copyright (C) 2024 OctHe
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all;
 close all;
 
-N = 100;
+N = 1000;
 BW = 20; % MHz
 fc = 2.4e9; % Hz
 d = 100; % m
@@ -18,16 +18,23 @@ v = 1; % m/s
 Ltx = 12; % cm
 Lrx = 12; % cm
 
+H = ChannelModel('F', BW, fc, d, Ntx, Ltx, Nrx, Lrx, v);
+
+% tic;
+% delta =  zeros(Ntx, N);
+% delta(:, 1) = 1 / sqrt(Ntx) * ones(Ntx, 1);
+% y = H.channel(delta);
+% figure;
+% plot((1: N) / BW, 20 * log10(abs(y.')));
+% xlabel('Time (us)')
+% ylabel('Power (dB)')
+% toc;
+
 tic;
-H = ChannelModel('B', BW, fc, d, Ntx, Ltx, Nrx, Lrx, v);
-
-x =  zeros(Ntx, N);
-x(:, 1) = 1 / sqrt(Ntx) * ones(Ntx, 1);
-y = H.channel(x);
-t = toc;
-t
+u = 1 / sqrt(Ntx) * ones(Ntx, N);
+y = H.channel(u);
 figure;
-plot((1: N) / BW, 20 * log10(abs(y.')));
+plot((1: N) / BW, 10 * log10(abs(y.')));
 xlabel('Time (us)')
-ylabel('Power (dB)')
-
+ylabel('Amplitude (dB)')
+toc;
